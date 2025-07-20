a6bde7cd268157bac11416b44196ce7479b7629b
Onur Simsek <posta@onursimsek.com>
2025-07-20 15:14:50 +0300

    Add deposit method to payment class

--- src/Requests/Payment.php
+++ src/Requests/Payment.php
@@ -6,12 +6,17 @@ namespace OnurSimsek\Craftgate\Requests;
 
 use OnurSimsek\Craftgate\Concerns\ForwardCallsToDecorator;
 use OnurSimsek\Craftgate\Proxies\CardPaymentProxy;
+use OnurSimsek\Craftgate\Proxies\CheckoutPaymentProxy;
+use OnurSimsek\Craftgate\Proxies\DepositProxy;
 use OnurSimsek\Craftgate\Requests\Payments\CardPayment;
 use OnurSimsek\Craftgate\Requests\Payments\CheckoutPayment;
+use OnurSimsek\Craftgate\Requests\Payments\Deposit;
 
 /**
  * @mixin CardPaymentProxy
- */
+ * @mixin CheckoutPaymentProxy
+ * @mixin DepositProxy
+ */gs
 final class Payment extends AbstractRequestBridge
 {
     use ForwardCallsToDecorator;
@@ -19,6 +24,8 @@ final class Payment extends AbstractRequestBridge
     private CardPayment $cardPayment;
 
     private CheckoutPayment $checkoutPayment;
+
+    private Deposit $deposit;
 
     #[AsDecorator]
     public function cardPayment(): CardPayment
@@ -30,5 +37,11 @@ final class Payment extends AbstractRequestBridge
     public function checkoutPayment(): CheckoutPayment
     {
         return $this->checkoutPayment ??= new CheckoutPayment($this->request);
+    }
+
+    #[AsDecorator]
+    public function deposit(): Deposit
+    {
+        return $this->deposit ??= new Deposit($this->request);
     }
 }
