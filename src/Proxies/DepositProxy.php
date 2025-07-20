e15ccd3ae589d639821f44a6ec73ff3fb13a5926
Onur Simsek <posta@onursimsek.com>
2025-07-20 15:11:58 +0300

    Fix deposit proxy parameters

--- src/Proxies/DepositProxy.php
+++ src/Proxies/DepositProxy.php
@@ -6,6 +6,10 @@ namespace OnurSimsek\Craftgate\Proxies;
 
 use OnurSimsek\Craftgate\Contracts\Proxy;
 use OnurSimsek\Craftgate\Requests\Payments\Deposit;
+use OnurSimsek\Craftgate\ValueObjects\CreateApmDeposit;
+use OnurSimsek\Craftgate\ValueObjects\CreateDeposit;
+use OnurSimsek\Craftgate\ValueObjects\CreateFundTransfer;
+use OnurSimsek\Craftgate\ValueObjects\CreateThreeDSecureDeposit;
 use Psr\Http\Message\ResponseInterface;
 
 final class DepositProxy implements Proxy
@@ -16,26 +20,26 @@ final class DepositProxy implements Proxy
 
     public function createDepositPayment(array $params): ResponseInterface
     {
-        return $this->decorator->create($params);
+        return $this->decorator->create(CreateDeposit::fromArray($params));
     }
 
     public function init3DSDepositPayment(array $params): ResponseInterface
     {
-        return $this->decorator->createThreeDSecure($params);
+        return $this->decorator->createThreeDSecure(CreateThreeDSecureDeposit::fromArray($params));
     }
 
     public function complete3DSDepositPayment(array $params): ResponseInterface
     {
-        return $this->decorator->completeThreeDSecure($params);
+        return $this->decorator->completeThreeDSecure($params['paymentId']);
     }
 
     public function createFundTransferDepositPayment(array $params): ResponseInterface
     {
-        return $this->decorator->createFundTransfer($params);
+        return $this->decorator->createFundTransfer(CreateFundTransfer::fromArray($params));
     }
 
     public function initApmDepositPayment(array $params): ResponseInterface
     {
-        return $this->decorator->createApm($params);
+        return $this->decorator->createApm(CreateApmDeposit::fromArray($params));
     }
 }
