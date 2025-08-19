2e46cd5089295e3f4be0ecce3470568b30100d05
Onur Şimşek <posta@onursimsek.com>
2025-08-19 22:33:44 +0300

    Add pos apm endpoints (#14)
    * Add pos apm endpoints
    
    * Fix styling
    
    ---------
    
    Co-authored-by: onursimsek <1241396+onursimsek@users.noreply.github.com>

---
+++ src/Enums/PaymentProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnurSimsek\Craftgate\Enums;
+
+enum PaymentProvider: string
+{
+    case GarantiPay = 'GARANTI_PAY';
+    case WorldPay = 'YKB_WORLD_PAY';
+    case WorldPayShoppingLoan = 'YKB_WORLD_PAY_SHOPPING_LOAN';
+}
---
+++ src/Requests/Payments/PosApmPayment.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnurSimsek\Craftgate\Requests\Payments;
+
+use OnurSimsek\Craftgate\Concerns\Container;
+use OnurSimsek\Craftgate\Contracts\Proxy;
+use OnurSimsek\Craftgate\Proxies\PosApmPaymentProxy;
+use OnurSimsek\Craftgate\Requests\HttpVerb;
+use OnurSimsek\Craftgate\Requests\RequestDecorator;
+use OnurSimsek\Craftgate\ValueObjects\InitializePosApmPayment;
+use Psr\Http\Message\ResponseInterface;
+
+class PosApmPayment extends RequestDecorator
+{
+    protected string $prefix = 'payment';
+
+    public function init(InitializePosApmPayment $payment): ResponseInterface
+    {
+        return $this->withMethod(HttpVerb::Post)
+            ->withPath('pos-apm-payments', 'init')
+            ->withBody($payment->toArray())
+            ->send();
+    }
+
+    public function complete(int $paymentId): ResponseInterface
+    {
+        return $this->withMethod(HttpVerb::Post)
+            ->withPath('pos-apm-payments', 'complete')
+            ->withBody(['paymentId' => $paymentId])
+            ->send();
+    }
+
+    protected function proxy(): Proxy
+    {
+        return Container::pushOrGet(PosApmPaymentProxy::class, $this);
+    }
+}
