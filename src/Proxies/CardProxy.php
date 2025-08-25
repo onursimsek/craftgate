598c5b71e91a4a774845f27e37577cdd3d9f8046
Onur Simsek <posta@onursimsek.com>
2025-08-25 16:07:51 +0300

    Fix proxy parameters

--- src/Proxies/CardProxy.php
+++ src/Proxies/CardProxy.php
@@ -6,6 +6,9 @@ namespace OnurSimsek\Craftgate\Proxies;
 
 use OnurSimsek\Craftgate\Requests\Payments\Card;
 use OnurSimsek\Craftgate\Contracts\Proxy;
+use OnurSimsek\Craftgate\ValueObjects\CreateStoredCard;
+use OnurSimsek\Craftgate\ValueObjects\SearchStoredCard;
+use OnurSimsek\Craftgate\ValueObjects\UpdateStoredCard;
 use Psr\Http\Message\ResponseInterface;
 
 final class CardProxy implements Proxy
@@ -16,26 +19,21 @@ final class CardProxy implements Proxy
 
     public function storeCard(array $params): ResponseInterface
     {
-        return $this->decorator->create($params);
+        return $this->decorator->create(CreateStoredCard::fromArray($params));
     }
 
     public function updateCard(array $params): ResponseInterface
     {
-        return $this->decorator->update($params);
-    }
-
-    public function cloneCard(array $params): ResponseInterface
-    {
-        return $this->decorator->clone($params);
+        return $this->decorator->update(UpdateStoredCard::fromArray($params));
     }
 
     public function searchStoredCards(array $params): ResponseInterface
     {
-        return $this->decorator->search($params);
+        return $this->decorator->search(SearchStoredCard::fromArray($params));
     }
 
     public function deleteStoredCard(array $params): ResponseInterface
     {
-        return $this->decorator->delete($params);
+        return $this->decorator->delete($params['cardUserKey'], $params['cardToken']);
     }
 }
